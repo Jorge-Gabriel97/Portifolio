e14c41fa60f5b4e3247dee7a7f7fa7a611509639
Jorge Gabriel <jg3043505@gmail.com>
2026-03-02 16:47:27 -0300

    Button_Mobile

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import { Link } from "react-router-dom";
 //Assets
 import Logo from "../../assets/Logo.png";
@@ -7,27 +8,32 @@ import "./Header.css";
 import Button from '../Button/Button';
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu =() => {
+        setIsOpen(!isOpen);
+    }
     return (
         <header>
-            <div className="container">
+            <div className="container">                
                 <div className="al-center d-flex jc-space-between">
                     <Link to="/"><img src={Logo} alt="Logo Jorge" /></Link>
                     <div className="mobile-menu">
-                        <Button buttonStyle="secondary">
-                            Menu
-                        </Button>
+                        <Button buttonStyle="secondary" onClick={toggleMenu}>Menu</Button>
                     </div>
+                    <nav className={`${isOpen ? 'open' : ''}`}>
+                        <Button buttonStyle="unstyled" className="mobile-menu close-btn" onClick={toggleMenu}>
+                            x
+                        </Button>
+                        <ul className="d-flex">
+                            <li><Link to="/">Inicio</Link></li>
+                            <li><Link to="/about">Sobre mim</Link></li>
+                            <li><Link to="/Projects">Projetos</Link></li>
+                            <li><Link to="/contact">Contato</Link></li>
+                        </ul>
+                    </nav>
                 </div>
-                <nav>
-                    <ul className="d-flex">
-                        <li><Link to="/">Inicio</Link></li>
-                        <li><Link to="/about">Sobre mim</Link></li>
-                        <li><Link to="/Projects">Projetos</Link></li>
-                        <li><Link to="/contact">Contato</Link></li>
-                    </ul>
-                </nav>
             </div>
-        </header >
+        </header>
     )
 }
 
